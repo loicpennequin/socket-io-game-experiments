1cf1b2b66338f684d4168a304acf19d7fc3bc9d5
loicpennequin <lo.pennequin@gmail.com>
2022-11-09 22:53:29 +0100

    trying to fix deploy

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,13 +3,20 @@ import http from 'http';
 import { createGameMap } from './factories/gameMap';
 import { createGameServer } from './factories/gameServer';
 import { createGameWorld } from './factories/gameWorld';
-import path from 'path';
 
 const app = express();
 const server = http.createServer(app);
 const world = createGameWorld({ map: createGameMap() });
 const gameServer = createGameServer(server, world);
 
-app.use(express.static(path.join(process.cwd(), '../client/dist')));
+app.get('*', (req, res, next) => {
+  console.log(req.url);
+
+  next();
+});
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('public'));
+}
 
 gameServer.start(port => console.log(`listening on port ${port}`));
