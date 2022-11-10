797ccc95967bf84c00315099d63897fd2a0a4875
loicpennequin <lo.pennequin@gmail.com>
2022-11-10 06:43:29 +0100

    force websockets transport

--- packages/client/src/utils/socket.ts
+++ packages/client/src/utils/socket.ts
@@ -6,5 +6,5 @@ import { io, type Socket } from 'socket.io-client';
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
   import.meta.env.VITE_SERVER_URL,
-  { autoConnect: false }
+  { autoConnect: false, transports: ['websocket'] }
 );
